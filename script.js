5187922e955a56c3f7fcd71f6ff193bd39b6ca13
Lance <lanceandreiducante@gmail.com>
2026-06-28 19:58:14 +0800

    created hero section

--- script.js
+++ script.js
@@ -50,3 +50,24 @@ const sectionObserver = new IntersectionObserver(
 sections.forEach((section) => {
   sectionObserver.observe(section);
 });
+
+const animatedItems = document.querySelectorAll('.animate-item');
+
+const animationObserver = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('show');
+      } else {
+        entry.target.classList.remove('show');
+      }
+    });
+  },
+  {
+    threshold: 0.2,
+  },
+);
+
+animatedItems.forEach((item) => {
+  animationObserver.observe(item);
+});
